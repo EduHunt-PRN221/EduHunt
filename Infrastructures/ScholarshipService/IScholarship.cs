2da014bb8f3e0608d3069d921120a5dae9ae8a29
thanhvjppro <trantienthanh23062003@gmail.com>
2024-09-15 16:08:27 +0700

    Add function

---
+++ Infrastructures/ScholarshipService/IScholarship.cs
@@ -0,0 +1,12 @@
+﻿using Eduhunt.DTOs;
+using EDUHUNT_BE.Models;
+
+namespace Eduhunt.Infrastructures.Repositories
+{
+    public interface IScholarship
+    {
+        Task<List<ScholarshipDto>> GetScholarships();
+        Task AddScholarship(ScholarshipDto scholarship);
+        Task<List<Scholarship>> GetRecommendedScholarships(string userId);
+    }
+}
