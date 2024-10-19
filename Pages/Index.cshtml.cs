723ec775059c0a780872df175a331d56e532491e
Nguyễn Tiến Thành <88380769+Thanhhoacam@users.noreply.github.com>
2024-10-19 09:53:17 +0700

    search scholarship and using province api

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -9,13 +9,12 @@ namespace Eduhunt.Pages
         public IQueryable<Scholarship> NewScholarships = default!;
         public IEnumerable<Scholarship> TopScholarships = default!;
         private readonly IServiceProvider _serviceProvider;
-
         public IndexModel(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
         }
 
-        public void OnGet()
+        public void OnGet(string SchoolName, string Country, string City, string Major, string Level, string Budget)
         {
             var scholarship = _serviceProvider.GetService<ScholarshipService>();
             if (scholarship != null)
@@ -25,6 +24,29 @@ namespace Eduhunt.Pages
                 Random random = new Random();
                 TopScholarships = allScholarship.ToList().OrderBy(x => x.Budget)
                                 .Take(4);
+                //search
+                if (NewScholarships != null)
+                {
+                    if (!string.IsNullOrEmpty(SchoolName))
+                    {
+                        NewScholarships = NewScholarships.Where(x => x.SchoolName != null && x.SchoolName.ToLower().Contains(SchoolName.ToLower()));
+                    }
+
+                    if (!string.IsNullOrEmpty(Country) || !string.IsNullOrEmpty(City))
+                    {
+                        NewScholarships = NewScholarships.Where(x => x.Location != null && (x.Location.ToLower().Contains(Country.ToLower()) || x.Location.ToLower().Contains(City.ToLower())));
+                    }
+
+                    if (!string.IsNullOrEmpty(Level))
+                    {
+                        NewScholarships = NewScholarships.Where(x => x.Level != null && x.Level.Contains(Level.ToLower()));
+                    }
+
+                    if (!string.IsNullOrEmpty(Budget))
+                    {
+                        NewScholarships = NewScholarships.Where(x => x.Budget != null && x.Budget.Contains(Budget.ToLower()));
+                    }
+                }
             }
         }
     }
